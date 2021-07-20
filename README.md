1b558803df11995a9cd400c34c56208f860f3376
Tano Rojas <lisandro@compucorp.co.uk>
2021-07-20 20:12:55 +0700

    CPS-603: Add base files

---
+++ README.md
@@ -0,0 +1,24 @@
+# CiviCRM ApiLimitSettings
+
+## What is ApiLimitSettings?
+By default, CiviCRM adds a limit of 25 to the number of results returned by any API call, if not other is specified.
+
+This default behaviour has caused some errors, since the calls without limit specified are in most of the cases expecting to receive all the existing records.
+
+ApiLimitSettings modifies that default limit, making it equals to 0, meaning that all the results will be returned.
+
+
+## How do I get ApiLimitSettings?
+
+### Installation
+If this is the first time you use an CiviCRM extension, please see [Here](http://wiki.civicrm.org/confluence/display/CRMDOC/Extensions "CiviCRM Extensions Installation") for full instructions and information on how to set and configure extensions.
+
+You can get the latest release of ApiLimitSettings from [Github repository release page](https://github.com/compucorp/uk.co.compucorp.civicrm.apilimitsettings/releases).
+
+Contact info@compucorp.co.uk if you need any assistance with installation.
+
+
+## Support
+Please contact the following email if you have any question: <hello@compucorp.co.uk>
+
+Paid support for this extension is available, please contact us either via Github or at <hello@compucorp.co.uk>
---
+++ apilimitsettings.php
@@ -0,0 +1,155 @@
+<?php
+
+require_once 'apilimitsettings.civix.php';
+// phpcs:disable
+use CRM_Apilimitsettings_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function apilimitsettings_civicrm_config(&$config) {
+  _apilimitsettings_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
+ */
+function apilimitsettings_civicrm_xmlMenu(&$files) {
+  _apilimitsettings_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function apilimitsettings_civicrm_install() {
+  _apilimitsettings_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function apilimitsettings_civicrm_postInstall() {
+  _apilimitsettings_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function apilimitsettings_civicrm_uninstall() {
+  _apilimitsettings_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function apilimitsettings_civicrm_enable() {
+  _apilimitsettings_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function apilimitsettings_civicrm_disable() {
+  _apilimitsettings_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function apilimitsettings_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _apilimitsettings_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function apilimitsettings_civicrm_managed(&$entities) {
+  _apilimitsettings_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
+ */
+function apilimitsettings_civicrm_caseTypes(&$caseTypes) {
+  _apilimitsettings_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
+ */
+function apilimitsettings_civicrm_angularModules(&$angularModules) {
+  _apilimitsettings_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
+ */
+function apilimitsettings_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _apilimitsettings_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function apilimitsettings_civicrm_entityTypes(&$entityTypes) {
+  _apilimitsettings_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_thems().
+ */
+function apilimitsettings_civicrm_themes(&$themes) {
+  _apilimitsettings_civix_civicrm_themes($themes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function apilimitsettings_civicrm_preProcess($formName, &$form) {
+//
+//}
